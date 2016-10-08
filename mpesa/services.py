f4de5d0c4a6c3563413ea7036a6d3c7fd3e0e132
Loek van Gent <loek@onepercentclub.com>
2016-10-08 13:25:29 +0200

    Get demo request going

--- mpesa/services.py
+++ mpesa/services.py
@@ -1,30 +1,45 @@
-from datetime import datetime
+import time
 import suds
 import hashlib
 import base64
 import xml.etree.ElementTree as ET
 
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logging.getLogger('suds').setLevel(logging.WARN)
+
 
 class PaymentService(object):
 
     wsdl = 'https://www.safaricom.co.ke/mpesa_online/lnmo_checkout_server.php?wsdl'
+    demo = False
+    demo_merchant_id = '898998'
+    demo_timestamp = '20160510161908'
+    demo_password = 'ZmRmZDYwYzIzZDQxZDc5ODYwMTIzYjUxNzNkZDMwMDRjNGRkZTY2ZDQ3ZTI0YjVjODc4ZTExNTNjMDA1YTcwNw=='
 
-    def __init__(self, merchant_id, merchant_passkey, debug=False):
-        self.merchant_id = merchant_id
+    def __init__(self, merchant_id='demo', merchant_passkey='demo', debug=False):
+        if merchant_id == 'demo':
+            self.demo = True
+            self.merchant_id = self.demo_merchant_id
+        else:
+            self.merchant_id = merchant_id
+
         self.merchant_passkey = merchant_passkey
         self.debug = debug
-        # self.client = suds.client.Client(self.wsdl)
-        #
-        # # Make sure locations for methods are correctly set
-        # self.client.service.processCheckOut.method.location = self.wsdl
-        # self.client.service.confirmTransaction.method.location = self.wsdl
-        # self.client.service.LNMOResult.method.location = self.wsdl
-        # self.client.service.transactionStatusQuery.method.location = self.wsdl
+        self.client = suds.client.Client(self.wsdl)
 
-    def _make_password(self, timestamp):
-        if not timestamp:
-            timestamp = datetime.now()
-        return base64.b64encode(hashlib.sha256(self.merchant_id + self.merchant_passkey + timestamp).hexdigest().upper())
+        # Make sure locations for methods are correctly set
+        self.client.service.processCheckOut.method.location = self.wsdl
+        self.client.service.confirmTransaction.method.location = self.wsdl
+        self.client.service.LNMOResult.method.location = self.wsdl
+        self.client.service.transactionStatusQuery.method.location = self.wsdl
+
+    def _generate_password(self, timestamp):
+        if self.demo:
+            return self.demo_password
+        key = "{0}{1}{2}".format(self.merchant_id, self.merchant_passkey, timestamp)
+        return base64.b64encode(hashlib.sha256(key).hexdigest().upper())
 
     def _parse_checkout_response(self, xml):
         root = ET.fromstring(xml)
@@ -48,7 +63,6 @@ class PaymentService(object):
             'details': res.findtext('ENC_PARAMS')
         }
 
-
     def _parse_payment_response(self, xml):
         root = ET.fromstring(xml)
         ns = {
@@ -71,5 +85,37 @@ class PaymentService(object):
             'details': res.findtext('ENC_PARAMS')
         }
 
-    def _parseXMLResponse(self, op, xml):
-        data = None
+    def _request_header(self, timestamp):
+        data = self.client.factory.create('tns:CheckOutHeader')
+        data.MERCHANT_ID = self.merchant_id
+        data.TIMESTAMP = timestamp
+        data.PASSWORD = self._generate_password(timestamp=timestamp)
+        return data
+
+    def checkout_request(self):
+        merchant_transaction_id = 'order-9876-{0}'.format(time.time())
+        reference_id = 'project-1234'
+        msisdn = '254700000000'
+        amount = 500
+        enc_params = None
+        callback_url = 'http://localhost:8000/payments_mpesa/status'
+        callback_method = 'xml'
+        if self.demo:
+            timestamp = self.demo_timestamp
+        else:
+            timestamp = time.time()
+
+        header = self._request_header(timestamp=timestamp)
+        self.client.set_options(soapheaders=header)
+
+        response = self.client.service.processCheckOut(
+            merchant_transaction_id,
+            reference_id,
+            amount,
+            msisdn,
+            enc_params,
+            callback_url,
+            callback_method,
+            timestamp
+            )
+        return response
--- mpesa/tests/test_service.py
+++ mpesa/tests/test_service.py
@@ -4,10 +4,20 @@ from mpesa.services import PaymentService
 
 
 class TestReponse(TestCase):
-    def test_is_string(self):
+
+    def test_response(self):
         service = PaymentService(merchant_id=123, merchant_passkey='topsecret')
         file = open('mpesa/tests/files/response-success.xml')
         xml = file.read()
         data = service._parse_checkout_response(xml)
         self.assertEqual(data['description'], 'Success')
         self.assertEqual(data['mpesa_txn'], 'cce3d32e0159c1e62a9ec45b67676200')
+
+    def test_request(self):
+        service = PaymentService(merchant_id='demo', merchant_passkey='demo')
+        response = service.checkout_request()
+        self.assertEqual(response.DESCRIPTION, 'Success')
+        self.assertEqual(response.RETURN_CODE, '00')
+        self.assertEqual(response.CUST_MSG, ("To complete this transaction, enter your "
+                                             "PIN on your handset. If you don't have a "
+                                             "PIN, press 0 and follow the instructions."))
